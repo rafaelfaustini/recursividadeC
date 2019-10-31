b5b027d10f7d7124d6519a92f8f104fc0f2c0def
Alex Salgado <alexsalgado1@gmail.com>
2019-10-31 19:08:05 -0300

    melhorando a interface

--- fibonacci/fibonacci_index.cpp
+++ fibonacci/fibonacci_index.cpp
@@ -3,6 +3,11 @@ int fibonacci_index(int pos){
 	if(pos==0 || pos == 1) return 1; 
 	return fibonacci_index(pos-1)+fibonacci_index(pos-2);
 }
+
 int main(){
-	printf("%d",fibonacci_index(10));		
+
+        printf("Sequencia de Fibonacci de forma recursiva:\n");
+	printf("%d\n\n",fibonacci_index(10));		
+
+	return 0;
 }
